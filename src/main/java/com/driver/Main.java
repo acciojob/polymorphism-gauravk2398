d0bee94113860c3deec3435e17bbdf43377e568d
akashsInterra <109330154+akashsInterra@users.noreply.github.com>
2023-01-12 23:44:47 +0530

    first submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+static class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double t, double v) {
+        return t*v;
+    }
+}
+public static void main(String args[]){
+    Product p= new Product();
+    p.product(4,5);
+    p.product(1,2,3);
+    p.product(345,0.5);
+}
 }
